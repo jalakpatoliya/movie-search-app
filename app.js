cd1ad718affea3f21a60d1871ab941355d6bd949
jalakpatoliya <30546867+jalakpatoliya@users.noreply.github.com>
2018-11-28 02:36:28 +0530

    updated

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
-var express = require("express");
-var app =express();
-var request = require("request");
+const express = require('express');
 
-app.get("/",function(req,res){
-  res.render("search.ejs");
-})
+const app = express();
+const request = require('request');
 
-app.get("/results",function(req,res){
-  var query = req.query.search;
-  var url ="http://theapache64.xyz:8080/movie_db/search?keyword="+query;
-  request(url,function(error,response,body){
-    if (!error && response.statusCode == 200) {
-      var parsedBody = JSON.parse(body);
-      res.render("results.ejs",{movieDetails:parsedBody});
+const key = '6bd8080b';
+const port = process.env.PORT || 3823;
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', (req, res) => {
+  // res.render('search.ejs');
+  res.redirect('/movieLists?search=baby');
+});
+
+app.get('/movieLists', (req, res) => {
+  const query = req.query.search;
+  const url = `http://www.omdbapi.com/?s=${query}&apikey=${key}`;
+  request(url, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const parsedBody = JSON.parse(body);
+      res.render('movieLists.ejs', { movieDetails: parsedBody });
     }
-  })
+  });
+});
 
-})
+app.get('/movie/:id', (req, res) => {
+  const id = req.params.id;
+  const url = `http://www.omdbapi.com/?i=${id}&apikey=${key}`;
+  request(url, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const parsedBody = JSON.parse(body);
+      res.render('results.ejs', { movieDetails: parsedBody });
+    }
+  });
+});
 
-app.listen(3823,function(){
-  console.log("movie search app started")
-})
+app.listen(port, () => {
+  console.log(`movie search app started at ${port}`);
+});
